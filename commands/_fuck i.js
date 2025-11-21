37748e9727e45dd68b3199ed8b727b174c92f7fa
BotAJS Deploy <dannyshedrack35@gmail.com>
2025-11-21 14:21:12 +0300

    Update bot commands

---
+++ commands/_fuck i.js
@@ -0,0 +1,7 @@
+/**
+ * Command: /fuck i
+ * Created: 2025-11-20 17:43:53.331633
+ * Updated: 2025-11-20 17:43:53
+ */
+
+// No code provided
---
+++ commands/_start.js
@@ -0,0 +1,7 @@
+/**
+ * Command: /start
+ * Created: 2025-11-18 16:02:14.237442
+ * Updated: 2025-11-19 08:13:32
+ */
+
+await ctx.reply("Hello there 👋");
